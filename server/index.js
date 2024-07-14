2e242cc21ba79166527a4da6b36c11c371804060
hammondcn <bayesianifeg1@gmail.com>
2024-07-14 19:11:02 +0800

    feat: 断点续传和暂停上传

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const logger = require("morgan");
 const path = require("path");
 const fs = require("fs-extra");
-const fsN = require("fs");
 const { StatusCodes } = require("http-status-codes");
 
 const CHUNK_SIZE = 100 * 1024 * 1024;
@@ -28,6 +27,7 @@ app.post("/upload/:filename", async (req, res, next) => {
   const { filename } = req.params;
   // 从查询参数取出分片索引
   const { chunkindex } = req.query;
+
   // 定义存放上传分片的目录
   const chunkDir = path.resolve(TEMP_DIR, filename);
   // 定义存放分片的路径
@@ -35,8 +35,10 @@ app.post("/upload/:filename", async (req, res, next) => {
   const chunkPath = path.resolve(chunkDir, chunkName);
   // 确保存放分片目录存在
   await fs.ensureDir(chunkDir);
+  // 写入文件的起始位置
+  const start = isNaN(req.query.start) ? 0 : parseInt(req.query.start, 10);
   // 创建文件的可写流
-  const ws = fs.createWriteStream(chunkPath, { flags: "a" });
+  const ws = fs.createWriteStream(chunkPath, { start, flags: "a" });
   // 以管道的方式把请求体中的流数据写入到文件中
   try {
     await pipeStream(req, ws);
@@ -58,12 +60,39 @@ app.get("/merge/:filename", async (req, res, next) => {
   }
 });
 
+// 提供一个接口返回已经上传了的分片和大小
+app.get("/verify/:filename", async (req, res) => {
+  // 从路径参数中获取文件名
+  const { filename } = req.params;
+  // 文件是否已经在服务端存在
+  const filePath = path.resolve(PUBLIC_DIR, filename);
+  const existFile = await fs.pathExists(filePath);
+  // debugger;
+  // 如果存在则不需要上传
+  if (existFile) {
+    return res.json({ success: true, needUpload: false });
+  }
+  const chunksDir = path.resolve(TEMP_DIR, filename);
+  const existDir = await fs.pathExists(chunksDir);
+  let uploadedChunkList = [];
+  if (existDir) {
+    const chunksName = await fs.readdir(chunksDir);
+    uploadedChunkList = await Promise.all(
+      chunksName.map(async (chunkName) => {
+        const chunkPath = path.resolve(chunksDir, chunkName);
+        const { size } = await fs.stat(chunkPath);
+        return { uploadedChunkName: chunkName, size };
+      }),
+    );
+  }
+  res.json({ success: true, needUpload: true, uploadedChunkList });
+});
+
 async function mergeChunks(filename) {
   const chunksDir = path.resolve(TEMP_DIR, filename);
 
   const chunksName = await fs.readdir(chunksDir);
-  // debugger;
-  console.log(chunksName);
+  debugger;
 
   // 去除mac系统生成的文件名
   // chunksName.filter((chunkNamnpe) => chunkName !== ".DS_Store");
@@ -84,6 +113,7 @@ async function mergeChunks(filename) {
       fs.createReadStream(chunkPath, { autoClose: true }),
       fs.createWriteStream(mergePath, {
         start,
+        falgs: "a",
       }),
     );
   });
@@ -95,7 +125,7 @@ async function mergeChunks(filename) {
 }
 
 function pipeStream(rs, ws) {
-  debugger;
+  // debugger;
   return new Promise((resolve, reject) => {
     rs.pipe(ws).on("finish", resolve).on("error", reject);
   });
